051127c7f4c3fd7379156c950fd2484119eefe00
tekin <tekin.omerali@worldpay.com>
2024-10-24 15:04:14 +0300

    Jenkinsfile

--- src/test/java/com/example/hello_world/HelloWorldApplicationTests.java
+++ src/test/java/com/example/hello_world/HelloWorldApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import static java.lang.StringTemplate.STR;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
